db7c3e10e659170bc4f954e98ce478030677bc66
Sacha <s.ruhe@atabix.nl>
2026-06-25 21:24:04 +0200

    remove stupid checks

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-npm audit --prefix backend --audit-level high
-
 export BACKEND_BASE_PATH=${BASE_PATH:-/gifselector}
 export VITE_BASE_PATH=${BACKEND_BASE_PATH}
 find /app/frontend/dist -type f -exec sed -i "s|/gifselector|${BACKEND_BASE_PATH}|g" {} +
